8ddf9ba94a5f2064cb789bb53fdb1d99efd57747
kagemiku <kagemiku@gmail.com>
2019-05-03 21:39:32 +0900

    Init xcode project

---
+++ cocoda-impls/cocoda-impls/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cocoda-impls
+//
+//  Created by KAGE on 2019/05/03.
+//  Copyright © 2019 kagemiku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
