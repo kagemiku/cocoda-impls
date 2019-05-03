a797992a4026252e6f5818543d0687beb7505339
kagemiku <kagemiku@gmail.com>
2019-05-03 22:22:04 +0900

    Add xib for ECProductVC

--- cocoda-impls/cocoda-impls/SampleImpls/IntroECProduct/ECProductViewController.swift
+++ cocoda-impls/cocoda-impls/SampleImpls/IntroECProduct/ECProductViewController.swift
@@ -12,7 +12,6 @@ class ECProductViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.blue
 
         // Do any additional setup after loading the view.
     }
